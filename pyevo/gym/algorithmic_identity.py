b48536c586ae2f85f7462e34309a3d3ce6cb3e83
Nándor Varga <nandor55587@gmail.com>
2020-10-12 23:42:56 +0200

    Add algorithmic identity definition

---
+++ pyevo/gym/algorithmic_identity.py
@@ -0,0 +1,29 @@
+import random
+
+
+class AlgorithmicIdentity:
+
+    def __init__(self):
+        self._task = tuple(random.randint(1, 1001) for _ in range(1000))
+
+    @property
+    def task(self):
+        """The array to submit as solution
+
+        This array contains 1000 integers ranging from 1 to 1001 which can be read as many times as needed and must be
+        submitted as a solution. This value is generated at class initialization and regenerated each time a solution
+        is submitted.
+        """
+        return self._task
+
+    def submit_solution(self, solution):
+        if len(solution) != 1000:
+            return 0
+        score = int()
+        for element in zip(self._task, solution):
+            if element[0] == element[1]:
+                score += 1
+        return score/1000
+
+    ...
+
