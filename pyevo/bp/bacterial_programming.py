3ab2b48cd1c44db1224308e5b70e5ce21dbc45b0
Nándor Varga <nandor55587@gmail.com>
2020-10-13 21:45:19 +0200

    Add bacterial programming definition

---
+++ pyevo/bp/bacterial_programming.py
@@ -0,0 +1,26 @@
+from pyevo.bp.strategies import \
+    InitializationStrategy, TerminationStrategy, BacterialMutationStrategy, InfectionStrategy
+
+
+class BacterialProgramming:
+
+    @property
+    def results(self):
+        return list(self._results)
+
+    def __init__(self,
+                 initialization_strategy=InitializationStrategy(),
+                 termination_strategy=TerminationStrategy(),
+                 bacterial_mutation_strategy=BacterialMutationStrategy(),
+                 infection_strategy=InfectionStrategy()):
+        self._results = list()
+        self._initialization_strategy = initialization_strategy
+        self._termination_strategy = termination_strategy
+        self._bacterial_mutation_strategy = bacterial_mutation_strategy
+        self._infection_strategy = infection_strategy
+
+    def run(self):
+        population = self._initialization_strategy()
+        while not self._termination_strategy(population, self._results):
+            self._bacterial_mutation_strategy(population)
+            self._infection_strategy(population)
---
+++ pyevo/bp/specimen.py
@@ -0,0 +1,13 @@
+class Specimen:
+    @property
+    def fitness(self):
+        return 1
+
+    def __call__(self, task):
+        pass
+
+    def bacterial_mutation(self, clone_count):
+        pass
+
+    def infect(self, other):
+        pass
