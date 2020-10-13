430cfa0e091e6f76c9437eb18c557dddb52803ad
Nándor Varga <nandor55587@gmail.com>
2020-10-13 17:35:41 +0200

    Add algorithmic reverse file

--- pyevo/gym/algorithmic_reverse.py
+++ pyevo/gym/algorithmic_reverse.py
@@ -1,18 +1,18 @@
 import random
 
 
-class AlgorithmicIdentity:
+class AlgorithmicReverse:
 
     def __init__(self):
         self._task = tuple(random.randint(1, 1001) for _ in range(1000))
 
     @property
     def task(self):
-        """The array to submit as a solution
+        """The original array of which the reverse must be submitted
 
         This array contains 1000 integers ranging from 1 to 1000 inclusive which can be read as many times as needed and
-        must be submitted as a solution. This value is generated at class initialization and regenerated each time a
-        solution is submitted.
+        the reverse must be submitted as a solution. This value is generated at class initialization and regenerated
+        each time a solution is submitted.
         """
         return self._task
 
@@ -21,7 +21,7 @@ class AlgorithmicIdentity:
             return 0
 
         score = int()
-        for element in zip(self._task, solution):
+        for element in zip(reversed(self._task), solution):
             if element[0] == element[1]:
                 score += 1
         # Regenerate task
